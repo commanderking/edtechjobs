d73029c8b4f25e855b6644aee2deb4692dc137fc
Jeffrey King <jek582@mail.harvard.edu>
2021-01-18 13:40:16 -0500

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import { ThemeProvider, theme, CSSReset } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
       <CSSReset />

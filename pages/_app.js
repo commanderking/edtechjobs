c50d6d106684cffb56f4f7678b0c8f5bf3a75274
Jeffrey King <jek582@mail.harvard.edu>
2021-03-28 12:25:32 -0400

    update update date

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider theme={theme}>
       <CSSReset />
       <Box maxWidth="824px" margin="auto" padding="20px" mt={10}>
-        <Component {...pageProps} lastUpdate={new Date("3/12/2021")} />
+        <Component {...pageProps} lastUpdate={new Date("3/28/2021")} />
       </Box>
     </ThemeProvider>
   );
